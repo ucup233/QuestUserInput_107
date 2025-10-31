d26b74b02bb7d61db82b6b5e28a2acfda5027ad3
ucup233 <sufhidayat9@gmail.com>
2025-10-31 09:45:03 +0700

    Pembuatan horizontal divider

--- app/src/main/java/com/example/myapplication/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/TeksEdit.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
@@ -20,7 +22,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
+
 
 @Composable
 fun FormDataDiri(modifier: Modifier){
@@ -75,6 +79,14 @@ fun FormDataDiri(modifier: Modifier){
             onValueChange = {
                 txtAlamat = it
             }
+        )
+
+        HorizontalDivider(
+            modifier = Modifier.padding(
+                bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+                    R.dimen.padding_medium
+                )
+            )
         )
 
     }
