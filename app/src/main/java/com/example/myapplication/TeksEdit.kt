40f4511dd73df7af0e4e610a20bfdfea027959bf
ucup233 <sufhidayat9@gmail.com>
2025-10-31 10:16:55 +0700

    Pembuatan column di dalam Card untuk menampilkan data

--- app/src/main/java/com/example/myapplication/TeksEdit.kt
+++ app/src/main/java/com/example/myapplication/TeksEdit.kt
@@ -117,7 +117,15 @@ fun FormDataDiri(modifier: Modifier){
                 .height(height = 100.dp)
                 .width(width = 300.dp)
         ) {
-
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 5.dp, vertical = 15.dp),
+            )
+            {
+                Text(text = "Nama : " +nama, color = Color.White)
+                Text(text = "Gender : " +jenis, color = Color.White)
+                Text(text = "Alamat : " +alamat, color = Color.White)
+            }
         }
 
     }
