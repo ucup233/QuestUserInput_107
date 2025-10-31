36856f5280b2508ef9f538bd6d2e1a8e4cd8c3bf
ucup233 <sufhidayat9@gmail.com>
2025-10-31 09:23:06 +0700

    Pembuatan text field nama

--- app/src/main/java/com/example/myapplication/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/TeksEdit.kt
@@ -1,10 +1,23 @@
 package com.example.myapplication.ui.theme
 
 import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
+@Composable
 fun FormDataDiri(modifier: Modifier){
     var txtNama by remember { mutableStateOf("") }
     var txtAlamat by remember { mutableStateOf("") }
@@ -15,4 +28,23 @@ fun FormDataDiri(modifier: Modifier){
     var jenis by remember { mutableStateOf("") }
 
     val gender:List<String> = listOf("Laki-laki", "Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        OutlinedTextField(
+            value = txtNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+
+            label = { Text(text = "Nama Lengkap") },
+            onValueChange = {
+                txtNama = it
+            }
+        )
+
+
+
+    }
 }
