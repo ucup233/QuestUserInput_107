151b0140e6502a51c47c3a02067df03777f2ff89
ucup233 <sufhidayat9@gmail.com>
2025-11-01 15:00:58 +0700

    Pembuatan box untuk background

--- app/src/main/java/com/example/myapplication/FormPendaftaran.kt
+++ app/src/main/java/com/example/myapplication/FormPendaftaran.kt
@@ -1,9 +1,15 @@
 package com.example.myapplication
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun FormPendaftaran(modifier: Modifier) {
@@ -19,4 +25,13 @@ fun FormPendaftaran(modifier: Modifier) {
 
     val gender = listOf("Laki-laki", "Perempuan")
     val status = listOf("Janda", "Lajang", "Duda")
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF5E6F7)),
+        contentAlignment = Alignment.Center
+    ) {
+
+    }
 }
