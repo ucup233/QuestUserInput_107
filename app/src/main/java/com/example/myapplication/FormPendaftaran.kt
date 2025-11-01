bc96374e1b9c03dbe3ed6eb7b1f12d708ad746b7
ucup233 <sufhidayat9@gmail.com>
2025-11-01 15:04:24 +0700

    Pembuatan Card untuk form pengisian dan content di dalam column

--- app/src/main/java/com/example/myapplication/FormPendaftaran.kt
+++ app/src/main/java/com/example/myapplication/FormPendaftaran.kt
@@ -1,11 +1,15 @@
 package com.example.myapplication
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -50,7 +54,24 @@ fun FormPendaftaran(modifier: Modifier) {
                 color = Color.Black,
                 fontSize = 20.sp
             )
+            Card(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(0.9f),
+                colors = CardDefaults.cardColors(containerColor = Color.White),
+                shape = RoundedCornerShape(16.dp),
+                elevation = CardDefaults.cardElevation(6.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(24.dp),
+                    horizontalAlignment = Alignment.Start,
+                    verticalArrangement = Arrangement.Top
+                ) {
 
+                }
+            }
         }
     }
 }
