41b8b4306942fc4d6429f39e975c8b93b30997be
ucup233 <sufhidayat9@gmail.com>
2025-11-01 15:03:03 +0700

    Pembuatan text "Formulir Pendaftaran"

--- app/src/main/java/com/example/myapplication/FormPendaftaran.kt
+++ app/src/main/java/com/example/myapplication/FormPendaftaran.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -11,6 +14,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun FormPendaftaran(modifier: Modifier) {
@@ -35,6 +41,15 @@ fun FormPendaftaran(modifier: Modifier) {
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Text(
+                text = "Formulir Pendaftaran",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(all = 16.dp),
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                fontSize = 20.sp
+            )
 
         }
     }
