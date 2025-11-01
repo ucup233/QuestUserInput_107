6781893181217e9a6cd5cf842da90acbdc3dea57
ucup233 <sufhidayat9@gmail.com>
2025-11-01 15:05:58 +0700

    Pembuatan spacer antara nama dengan jenis kelamin

--- app/src/main/java/com/example/myapplication/FormPendaftaran.kt
+++ app/src/main/java/com/example/myapplication/FormPendaftaran.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -80,6 +82,7 @@ fun FormPendaftaran(modifier: Modifier) {
                             .fillMaxWidth()
                             .padding(vertical = 8.dp)
                     )
+                    Spacer(modifier = Modifier.height(16.dp))
                 }
             }
         }
