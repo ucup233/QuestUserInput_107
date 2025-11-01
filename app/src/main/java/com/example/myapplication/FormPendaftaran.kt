068d4478b678843392aefcf2a21e8d75fc5639db
ucup233 <sufhidayat9@gmail.com>
2025-11-01 14:58:15 +0700

    Pembuatan variabel untuk menyimpan input dari text field

--- app/src/main/java/com/example/myapplication/FormPendaftaran.kt
+++ app/src/main/java/com/example/myapplication/FormPendaftaran.kt
@@ -1,6 +1,14 @@
 package com.example.myapplication
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
 @Composable
 fun FormPendaftaran(modifier: Modifier) {
-
+    var textNama by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
 }
